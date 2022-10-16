e14af407a64b70a2afc303b7e922cdbad24237f8
Eniola Ademola <it.eniolaademola@gmail.com>
2022-10-16 18:40:00 +0100

    learning the basics of typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+// ========== TYPESCRIPT TUTORIAL ========== //
+
+let name: string; // use any for anytype value
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[]
+let role: [number, string]
+
+// ===== HAndling types in tsx ===== //
+
+// type Person = {
+//   name: string,
+//   age?: number
+// }
+
+type X = {
+  a: string,
+  b: number
+}
+
+type Y = X | {
+  c: string,
+  d: number
+}
+
+// when using this it must contain all thevalues is X and Y
+let y: Y = {
+  c: "enny",
+  d: 10
+}
+
+let person: Person = {
+  name: "Dave"
+}
+
+let lotsOfPeople: Person[]
+
+let personName : unknown; //instead of using any we use unknown its better that way
+
+// let printName: (name: string) => void; //dealing with functions in tsx
+
+// void return ondefined
+// never doesn't retun anything
+
+let printName = (name: string) => {
+  console.log("Hello ", name);
+}
+
+printName("Dave")
+
+// ===== HAndling interface in tsx ===== //
+
+interface Person {
+  name: string,
+  age?: number
+}
+
+interface Guy extends Person {
+  professsion: string
+}
+
+const App: React.FC = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='heading'>TASK-APP</span>
     </div>
   );
 }
