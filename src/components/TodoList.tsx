a3b12a15bcd60c943f8e4486d64f441eba76f511
Eniola Ademola <it.eniolaademola@gmail.com>
2022-10-18 13:44:33 +0100

    divided completed and active task

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,20 +7,35 @@ import SingleTodo from './SingleTodo';
 interface Props {
     todos: Todo[],
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+    completedTodos: Todo[],
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
 // We receiving props from App component, so we must specify the type of our props
 // to the Functional component 
 const TodoList: React.FC<Props> = ({todos, setTodos}) => {
   return (
-    <div className='todos'>
-        {
-            todos.map((todo) => {
-                return (
-                    <SingleTodo key={todo.id} todo={todo} todos={todos} setTodos={setTodos}/>
-                )
-            })  
-        }
+    <div className='container'>
+        <div className="todos active__task">
+            <span className="todos__heading">Active Task</span>
+            {
+                todos.map((todo) => {
+                    return (
+                        <SingleTodo key={todo.id} todo={todo} todos={todos} setTodos={setTodos}/>
+                    )
+                })  
+            }
+        </div>
+        <div className="todos completed__task">
+            <span className="todos__heading">Completed Task</span>
+            {
+                todos.map((todo) => {
+                    return (
+                        <SingleTodo key={todo.id} todo={todo} todos={todos} setTodos={setTodos}/>
+                    )
+                })  
+            }
+        </div>
     </div>
   )
 }
